ee8598f4f4320b18fd0c1aef9231714f97e2918e
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-03-16 16:17:35 +0700

    Create dynamic routes by passing params, edit and delete invoices

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -18,6 +18,7 @@ const FormSchema = z.object({
 
 // omitting since we don't have id/date yet
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -36,4 +37,28 @@ export async function createInvoice(formData: FormData) {
     // update cached route and redirect user
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    })
+
+    const amountInCents = amount * 100
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FORM invoices WHERE id = ${id}`
+    revalidatePath('/dashboard/invoices')
 }
