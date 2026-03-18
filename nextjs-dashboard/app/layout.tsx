ce86fb812e970de0875dcdfe9f93461d191b380d
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-03-18 12:33:28 +0700

    Add metadata to app using the Metadata API

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,14 +1,27 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+// metadata defined here will be inherited by all children that use it
+// Next.js will automatically add the title and metadata to the application
+export const metadata: Metadata = {
+    // use templates to avoid having to update every page after name change
+    title: {
+        template: '%s | Acme Dashboard', // %s will be replaced with the specific page title
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    )
 }
